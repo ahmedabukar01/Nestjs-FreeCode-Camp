7fc34cf875ce27617576d9003774c4971b51a0f4
ahmed <ahmedlearn01@gmail.com>
2023-02-05 10:57:57 +0300

    user decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserService } from './user.service';
 import { Request } from 'express';
 import { JwtGuard } from 'src/auth/Guards/jwt.guard';
+import { User } from 'src/auth/decorator/user.decorator';
 
 @Controller('users')
 export class UserController {
@@ -12,8 +13,8 @@ export class UserController {
   
     @UseGuards(JwtGuard)
     @Get('me')
-    getMe(@Req() req: Request){
-        console.log(req.user)
+    getMe(@User('user') user){
+        console.log(user)
         return this.userService.getMe()
     }
 }
